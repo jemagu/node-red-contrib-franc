7ade43adde2010b0328c7da2c1c226efee2225a9
Simon Hailes <btsimonh@googlemail.com>
2016-11-24 13:30:44 +0000

    Updated to franc 2.0.0 + added optional msg.francoptions on input message allowing for whiteList, blackList, minLength, and all: the ability to get the franc probabilities for all languages.

--- src/franc.js
+++ src/franc.js
@@ -1,17 +1,49 @@
-module.exports = function(RED) {
-    
-    const franc = require("franc");
-    
+'use strict';
+
+module.exports = function (RED) {
+
+    var franc = require("franc");
+
     function FrancNode(config) {
+        var _this = this;
+
         RED.nodes.createNode(this, config);
         this.minLength = config.minLength;
-        this.on('input', msg => {
+        this.on('input', function (msg) {
             if (typeof msg.payload === 'string') {
-                let francObj = {iso639_3: franc(msg.payload, {'minLength': this.minLength})};
+                var options = {};
+				var all = false;
+				options.minLength = _this.minlength;
+				if (msg.francoptions){
+					if (msg.francoptions.minLength){
+						options.minLength = msg.francoptions.minLength;
+					}
+					if (msg.francoptions.whiteList){
+						options.whiteList = msg.francoptions.whiteList;
+					}
+					if (msg.francoptions.blackList){
+						options.blackList = msg.francoptions.blackList;
+					}
+					if (msg.francoptions.all){
+						all = msg.francoptions.all;
+					}
+					
+				}
+				if (all){
+					var resall = franc.all(msg.payload, options );
+					var lang = "";
+					if (resall[0]){
+						lang = resall[0][0];
+					}
+					var francObj = { iso639_3: lang, all:resall };
+				} else {
+					var lang = franc(msg.payload, options );
+					var francObj = { iso639_3: lang };
+				}
                 msg.franc = francObj;
             }
-            this.send(msg);
+            _this.send(msg);
         });
     }
     RED.nodes.registerType("franc", FrancNode);
-}
+};
